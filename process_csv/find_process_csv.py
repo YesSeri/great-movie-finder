d1a8bc8ce93d43f29e4b6d44da6884803a5098e8
yesseri <henrik.zenkert@gmail.com>
2021-01-22 16:38:49 +0100

    Went through script with pylint

--- process_csv/find_process_csv.py
+++ process_csv/find_process_csv.py
@@ -1,3 +1,4 @@
+"This script downloads info about all IMDB movies and filters out all the bad ones"
 import os
 import urllib.request
 import gzip
@@ -9,6 +10,7 @@ import pandas as pd
 
 
 def delete(dir_path):
+    "Deletes folder at specified path"
     try:
         shutil.rmtree(dir_path)
         print(str(dir_path) + ' has been deleted.')
@@ -16,47 +18,52 @@ def delete(dir_path):
         print(str(dir_path) + " does not exist, and can't be deleted")
 
 
-def create_dir(folder):
-    if not os.path.exists(folder):
-        os.makedirs(folder)
-    print(str(folder) + ' has been created.')
+def create_dir(dir_path):
+    "Creates a dir at specified path, if it doesn't exist already"
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+    print(str(dir_path) + ' has been created.')
 
 
 def download(output_path, url):
+    "Downloads file from URL and saves at output_path"
     print('Beginning file download with urllib2: ' + str(output_path))
     urllib.request.urlretrieve(url, output_path)
     print(str(url) + ' has been downloaded.')
 
 
 def unzip(data):
+    "Recieves a dictonary containing name of zip file and name of output file name and unzips it"
     with gzip.open(data['zip_name'], 'r') as f_in, open(data['file_name'], 'wb') as f_out:
         shutil.copyfileobj(f_in, f_out)
         print(f_out.name + 'has been unzipped, from ' + f_in.name)
 
 
 def merge_data(path1, path2, output_path):
+    "Merges the two TSV files"
 
     file1 = pd.read_csv(path1, sep='\t', low_memory=False)
     file2 = pd.read_csv(path2, sep='\t', low_memory=False)
 
-    df = file1.merge(file2, on='tconst')
+    dataframe = file1.merge(file2, on='tconst')
 
-    dfMerged = df[['tconst', 'averageRating', 'numVotes', 'titleType']]
-    dfMerged.to_csv(output_path, index=False)
+    dataframe_merged = dataframe[['tconst', 'averageRating', 'numVotes', 'titleType']]
+    dataframe_merged.to_csv(output_path, index=False)
     print('Csv files have been merged.')
 
 
 def filter_data(output_file, input_file):
-    def isMovie(row):
-        if (row[3] == 'movie'):
+    "Filters the movies, keeping only the good ones, and saving them in a new file."
+    def is_movie(row):
+        if row[3] == 'movie':
             return True
         return False
 
-    def isGoodMovie(row):
+    def is_good_movie(row):
         try:
             rating = float(row[1])
-            numVotes = int(row[2])
-            if rating > 8.2 and numVotes > 1300:
+            num_votes = int(row[2])
+            if rating > 8.2 and num_votes > 1300:
                 return True
         except ValueError:
             print('Oops!', sys.exc_info()[0], 'occurred.')
@@ -67,10 +74,10 @@ def filter_data(output_file, input_file):
         header = next(reader)
         row_arr = [header]
         for row in reader:
-            if isMovie(row) and isGoodMovie(row):
+            if is_movie(row) and is_good_movie(row):
                 row_arr.append(row)
-        with open(output_file, 'w', newline='') as newFile:
-            writer = csv.writer(newFile)
+        with open(output_file, 'w', newline='') as new_file:
+            writer = csv.writer(new_file)
             writer.writerows(row_arr)
             print('Data has been filtered and written to file.')
 
