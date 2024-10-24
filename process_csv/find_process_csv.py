f40789a2b4b8a829a208c6c85a9b5b0f77f470c3
Henrik <henrik.zenkert@gmail.com>
2024-10-24 13:28:35 +0200

    reverted slightly to old version

--- process_csv/find_process_csv.py
+++ process_csv/find_process_csv.py
@@ -9,14 +9,28 @@ import sys
 from pathlib import Path
 import pandas as pd
 
-
-def delete(dir_path):
-    """Deletes folder at specified path"""
-    try:
-        shutil.rmtree(dir_path)
-        print(str(dir_path) + ' has been deleted.')
-    except OSError:
-        print(str(dir_path) + " does not exist, and can't be deleted")
+folder = Path.cwd() / 'data'
+fieldnames= ['averageRating',
+             'genres',
+             'isAdult',
+             'numVotes',
+             'originalTitle',
+             'primaryTitle',
+             'runtimeMinutes',
+             'startYear',
+             'tconst',
+             'titleType',
+             ]
+ratings = {
+        'file_name': folder / 'title.ratings.tsv',
+        'zip_name':  folder / 'title.ratings.tsv.gz',
+        'url': 'https://datasets.imdbws.com/title.ratings.tsv.gz',
+        }
+basics = {
+        'file_name': folder / 'title.basics.tsv',
+        'zip_name': folder / 'title.basics.tsv.gz',
+        'url': 'https://datasets.imdbws.com/title.basics.tsv.gz',
+        }
 
 
 def create_dir(dir_path):
@@ -25,14 +39,6 @@ def create_dir(dir_path):
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
     print(str(dir_path) + ' has been created.')
-
-
-def download(output_path, url):
-    """Downloads file from URL 
-    and saves at output_path"""
-    print('Beginning file download with urllib2: ' + str(output_path))
-    urllib.request.urlretrieve(url, output_path)
-    print(str(url) + ' has been downloaded.')
 
 
 def unzip(data):
@@ -46,121 +52,65 @@ def unzip(data):
 def merge_data(path1, path2, output_path):
     """Merges the two TSV files"""
 
-    file1 = pd.read_csv(path1, low_memory=False)
-    file2 = pd.read_csv(path2, low_memory=False)
+    file1 = pd.read_csv(path1, sep='\t', low_memory=False)
+    file2 = pd.read_csv(path2, sep='\t', low_memory=False)
 
     dataframe = file1.merge(file2, on='tconst')
 
-    dataframe_merged = dataframe[[#
-                                  'averageRating',
-                                  'endYear',
-                                  'genres',
-                                  'isAdult',
-                                  'numVotes',
-                                  'originalTitle',
-                                  'primaryTitle',
-                                  'runtimeMinutes',
-                                  'startYear',
-                                  'tconst',
-                                  'titleType',
-                                  ]]
-
-#
+    dataframe_merged = dataframe[fieldnames]
     dataframe_merged.to_csv(output_path, index=False)
     print('Csv files have been merged.')
 
 
-#def filter_data(output_file, input_file):
-#    """Filters the movies, keeping only the good ones, 
-#    and saving them in a new file."""
-#    def is_movie(row):
-#        if row[3] == 'movie':
-#            return True
-#        return False
-#
-#    def is_good_movie(row):
-#        try:
-#            rating = float(row[1])
-#            num_votes = int(row[2])
-#            if rating > 8.2 and num_votes > 1300:
-#                return True
-#        except ValueError:
-#            print('Oops!', sys.exc_info()[0], 'occurred.')
-#        return False
-#
-#    with open(input_file) as csvfile:
-#        reader = csv.reader(csvfile, delimiter=',', quotechar='|')
-#        header = next(reader)
-#        row_arr = [header]
-#        for row in reader:
-#            if is_movie(row) and is_good_movie(row):
-#                row_arr.append(row)
-#        with open(output_file, 'w', newline='') as new_file:
-#            writer = csv.writer(new_file)
-#            writer.writerows(row_arr)
-#            print('Data has been filtered and written to file.')
+def filter_data(output_file, input_file):
+    """Filters the movies, keeping only the good ones, 
+    and saving them in a new file."""
+    def is_movie(row):
+        return row['titleType'] == 'movie'
 
-def filter_data(input_file, output_file, filter_funcs):
+    def is_good_movie(row):
+        rating = float(row['averageRating'])
+        num_votes = int(row['numVotes'])
+        return rating > 8.2 and num_votes > 1300
+
     with open(input_file) as csvfile:
-        reader = csv.reader(csvfile, delimiter='\t')
-        header = next(reader)
-        row_arr = [header]
+        reader = csv.DictReader(csvfile)
+        row_arr = []
         for row in reader:
-            for func in filter_funcs:
-                if not func(row):
-                    break
+            if is_movie(row) and is_good_movie(row):
                 row_arr.append(row)
         with open(output_file, 'w', newline='') as new_file:
-            writer = csv.writer(new_file)
+            writer = csv.DictWriter(new_file, fieldnames=fieldnames)
+            writer.writeheader()
             writer.writerows(row_arr)
-            print(f'{input_file} data has been filtered and written to file.')
+            print('Data has been filtered and written to file.')
 
-def is_good_movie(row):
-    rating = float(row[1])
-    if rating > 8.2:
-        return True
-    return False
+def setup_teardown(func):
 
-def is_not_famous_movie(row):
-    num_votes = int(row[2])
-    if num_votes > 1300:
-        return True
-    return False
+    def inner1(*args, **kwargs):
+        arr = [ratings, basics]
+        shutil.rmtree(folder, ignore_errors=True)
+        print('creating folder')
+        create_dir(folder)
+        for el in arr:
+            print('Beginning file download with urllib2: ' + str(el['zip_name']))
+            urllib.request.urlretrieve(el['url'], el['zip_name'])
+            print(str(el['url']) + ' has been downloaded.')
+            unzip(el)
+        func(*args, **kwargs)
+        for el in arr:
+            os.remove(el['file_name'])
+            os.remove(el['zip_name'])
 
+    return inner1
 
-def is_movie(row):
-    if row[1] == 'movie':
-        return True
-    return False
+@setup_teardown
+def run(*args, **kwargs):
+    merged_path = folder / 'merged.csv'
+    merge_data(basics['file_name'], ratings['file_name'], merged_path)
+    processed_file = Path.cwd() / folder / 'processed_data.csv'
+    filter_data(processed_file, merged_path)
 
 
 if __name__ == '__main__':
-    folder = Path.cwd() / 'data'
-
-    ratings = {
-            'file_name': folder / 'title.ratings.tsv',
-            'zip_name':  folder / 'title.ratings.tsv.gz',
-            'url': 'https://datasets.imdbws.com/title.ratings.tsv.gz',
-            }
-    basics = {
-            'file_name': folder / 'title.basics.tsv',
-            'zip_name': folder / 'title.basics.tsv.gz',
-            'url': 'https://datasets.imdbws.com/title.basics.tsv.gz',
-            }
-    arr = [ratings, basics]
-
-    delete(folder)
-    create_dir(folder)
-    for el in arr:
-        download(el['zip_name'], el['url'])
-        unzip(el)
-
-    filtered_ratings_file =  Path.cwd() / folder/  'filtered_ratings.csv'
-    filtered_basics_file =Path.cwd() / folder/  'filtered_basics.csv'
-
-    filter_data(ratings['file_name'],filtered_ratings_file, [lambda row : is_good_movie(row), lambda row: is_not_famous_movie(row)])
-    filter_data(basics['file_name'],filtered_basics_file, [lambda row: is_movie(row)])
-    merged_path = folder / 'merged.csv'
-    merge_data(filtered_ratings_file, filtered_basics_file, merged_path)
-
-    processed_file = Path.cwd() / folder / 'processed_data.csv'
+    run()
