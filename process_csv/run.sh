2ec5a3b5511ea0ccb8fd5cd2b2c24cdcb90a1488
Henrik <henrik.zenkert@gmail.com>
2024-10-28 19:59:18 +0100

    enabled all

---
+++ process_csv/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+source ./venv/bin/activate 
+python3 download_unzip.py && echo 'downloaded zips and unzipped'
+echo 'Trying to merge and filter' && python3 merge_filter.py && echo 'Merged and filtered data'
+if [ -f movies.db ]; then
+    mv movies.db movies.db.bak 
+	echo 'File moved successfully to backup  movies.db.bak'
+else
+    echo 'movies.db not found, nothing to backup.'
+fi
+python3 create_db.py && echo 'Created db'
+python3 movies_to_genres.py && echo 'Inserted movies to genres many to many'
+python3 actors_to_movies.py && echo 'Inserted actors to movies many to many'
