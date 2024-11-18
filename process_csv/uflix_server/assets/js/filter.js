966b31c46e029432c09d9599e6b365791aee5961
Henrik <henrik.zenkert@gmail.com>
2024-11-18 17:46:44 +0100

    improved css html

--- process_csv/uflix_server/assets/js/filter.js
+++ process_csv/uflix_server/assets/js/filter.js
@@ -1,15 +1,40 @@
-const langAll = (input) => {
-	console.log(input);
-	console.log(input.checked);
-	if (input.checked) {
-		document.querySelectorAll(`label>input[type='checkbox']:not(#checkbox-all)`).forEach(el => el.checked = false);
-	}
-}
+const form = document.querySelector('.filter__form');
+const inputAll = document.querySelector('.language--all > input');
+const inputOther = document.querySelectorAll('.language--specific > input');
+const showLanguageButton = document.getElementsByClassName('language__button language__button--show')[0];
+const hideLanguageButton = document.getElementsByClassName('language__button language__button--hide')[0];
+const languageContainer = document.getElementsByClassName('language__container')[0];
+const showLanguages = (evt) => {
+    evt.preventDefault();
+    languageContainer.classList.remove('hidden');
+    showLanguageButton.classList.add('hidden');
+    hideLanguageButton.classList.remove('hidden');
+};
+const hideLanguages = (evt) => {
+    evt.preventDefault();
+    languageContainer.classList.add('hidden');
+    showLanguageButton.classList.remove('hidden');
+    hideLanguageButton.classList.add('hidden');
+};
+
+const langAll = () => {
+    if (inputAll.checked) {
+        for (const input of inputOther) {
+            input.checked = false;
+        }
+    }
+};
 const langOther = (input) => {
-	console.log(input);
-	if (input.checked) {
-		const checkboxAll = document.querySelector(`#checkbox-all`);
-		console.log(checkboxAll)
-		checkboxAll.checked = false;
-	}
-}
+    const {checked} = input;
+    console.log('lang-other:', {checked});
+    if (checked) {
+        inputAll.checked = false;
+    }
+};
+// add event listener
+
+inputOther.forEach(input => input.addEventListener('change', () => langOther(input)));
+inputAll.addEventListener('change', langAll);
+
+showLanguageButton.addEventListener('click', showLanguages);
+hideLanguageButton.addEventListener('click', hideLanguages);
